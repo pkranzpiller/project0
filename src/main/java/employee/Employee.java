4c283845edbf69045771078924ff35614baafe7a
Patrick Kranzpiller <patrick.kranzpiller@wsu.edu>
2019-07-17 09:53:42 -0500

    fixed employee login bug. Null pointer to account.

--- src/main/java/employee/Employee.java
+++ src/main/java/employee/Employee.java
@@ -7,6 +7,7 @@ public class Employee extends User {
 	private Account account;
 	
 	public Employee() {
+		this.account = new Account();
 		this.account.setBalance(0);
 		this.account.setStatus("open");
 	}
